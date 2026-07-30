0febbd1723a7b004cb9d71f466582fa4a4bc4269
NhorSengAn <149985473+NhorSengAn@users.noreply.github.com>
2026-07-30 13:50:00 +0700

    todo list exerceise style on file11-todo-list.js and 11-todo-list.html

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,27 +1,57 @@
-const todoList = [];
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2026-07-30",
+  },
+  {
+    name: "Make meal",
+    dueDate: "2026-07-30",
+  },
+];
 renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = "";
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+
+    const { name, dueDate } = todoObject;
+
+    const html = `
+      <div>
+        ${name}
+      </div>
+        <div>
+        ${dueDate}
+      </div>
+        <button onlick="
+         todoList.splice(${i}, 1);
+         renderTodoList(); 
+        " class = "delete-todo-button">Delete</button>
+      
+    `;
     todoListHTML += html;
   }
-
-  console.log(todoListHTML);
-
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
 
 //Input data value
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
+  const dateInputElement = document.querySelector(".js-date-input");
 
   const name = inputElement.value;
-  todoList.push(name);
-  console.log(todoList);
+  const dueDate = dateInputElement.value;
 
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate,
+    name,
+    dueDate, //shortand cut if the property and vairble name are the same;
+  });
   inputElement.value = "";
 
   renderTodoList();
